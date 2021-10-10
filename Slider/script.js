b667fbe1e83194f589b217c5555107b9774e72c9
Isaac-MCastanho <fgcastanho@hotmail.com>
2021-10-09 21:06:51 -0300

    Projeto slider

---
+++ Slider/script.js
@@ -0,0 +1,43 @@
+let totalSlides = document.querySelectorAll('.slider--item').length;
+let curretSlide = 0;
+
+
+
+document.querySelector('.slider--width').style.width = 
+    `calc(100vw * ${totalSlides} )`;
+
+document.querySelector('.slider--controls').style.height = 
+    `${document.querySelector('.slider').clientHeight}px`;
+
+
+
+
+
+
+function goPrev(){
+    curretSlide--;
+    if(curretSlide < 0){
+        curretSlide = totalSlides-1;
+    }
+    updateMargin();
+}
+
+
+function goNext(){
+    curretSlide++;
+    if(curretSlide >(totalSlides-1)){
+        curretSlide=0;
+    }
+    updateMargin();
+}
+
+
+function updateMargin(){
+    let sliderItemWidth = document.querySelector('.slider--item').clientWidth;
+    let newMargin = (curretSlide * sliderItemWidth);
+    document.querySelector('.slider--width').style.marginLeft =
+    `-${newMargin}px`;
+}
+
+
+setInterval(goNext, 5000)
